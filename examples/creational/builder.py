dcc22e9407ad0eba787bdec3bf7f1a47e82cd3f6
marreA <alemarrerodiaz@gmail.com>
2018-11-28 14:00:00 +0000

    Prototype pattern

--- examples/creational/builder.py
+++ examples/creational/builder.py
@@ -3,6 +3,8 @@
 
 # The builder pattern is an object creation software design pattern with the intentions of finding a solution to the telescoping constructor anti-pattern.
 
+# When to use: 
+#   When there could be several flavors of an object and to avoid the constructor telescoping. The key difference from the factory pattern is that; factory pattern is to be used when the creation is a one step process while builder pattern is to be used when the creation is a multi step process.
 
 class Burger:
     
---
+++ examples/creational/prototype.py
@@ -0,0 +1,25 @@
+# Create object based on an existing object through cloning.
+
+# The prototype pattern is a creational design pattern in software development. It is used when the type of objects to create is determined by a prototypical instance, which is cloned to produce new objects.
+
+# When to use:
+# When an object is required that is similar to existing object or when the creation would be expensive as compared to cloning.
+
+
+
+import copy
+
+class Sheep:
+    
+    def __init__(self, name, category = "Mountain Sheep"):
+        self.name = name
+        self.category = category
+    
+    def __repr__(self):
+        return 'Hi I am {}, and I am a {}'.format(self.name, self.category)
+        
+original = Sheep('Jolly')
+print(original)
+cloned = copy.copy(original)
+cloned.name = "Dolly"
+print(cloned)
